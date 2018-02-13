37f80a6846c9b4433dac9c9d955f64cbe18d2152
Calvin Vo <cvogit@gmail.com>
2018-02-13 00:02:42 -0800

    First mini game project with Unity.

---
+++ Pinball/Assets/Scripts/GameController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject Ball;
+	public GameObject EndGate;
+	public GameObject ScoreGate;
+	public GameObject ShootGate;
+	public GameObject RightBounceWall;
+
+	public Text LifeBoard;
+	public Text ScoreBoard;
+	private int Score = 0;
+	private int Life = 4;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Disable gate taht block the ball
+	public void DisableGateCollision() {
+		Physics2D.IgnoreCollision (Ball.gameObject.GetComponent<Collider2D> (), ScoreGate.gameObject.GetComponent<Collider2D> ());
+		Physics2D.IgnoreCollision (Ball.gameObject.GetComponent<Collider2D> (), ShootGate.gameObject.GetComponent<Collider2D> ());
+		Physics2D.IgnoreCollision (Ball.gameObject.GetComponent<Collider2D> (), RightBounceWall.gameObject.GetComponent<Collider2D> ());
+	}
+
+	// Enable gate to block the ball
+	public void EnableGateCollision() {
+		Physics2D.IgnoreCollision (Ball.gameObject.GetComponent<Collider2D> (), ScoreGate.gameObject.GetComponent<Collider2D> (), false);
+		Physics2D.IgnoreCollision (Ball.gameObject.GetComponent<Collider2D> (), ShootGate.gameObject.GetComponent<Collider2D> (), false);
+		Physics2D.IgnoreCollision (Ball.gameObject.GetComponent<Collider2D> (), RightBounceWall.gameObject.GetComponent<Collider2D> (), false);
+	}
+
+	public void EndGame() {
+		Instantiate (EndGate);
+	}
+
+	public void IncreaseScore(int incScore) {
+		Score += incScore;
+		UpdateScoreBoard ();
+	}
+
+	public void HandleDeath() {
+		Life--;
+		UpdateLifeBoard ();
+		ContinueGame ();
+
+		if (Life == 0)
+			EndGame ();
+	}
+
+	// Shoot the ball and continute teh game
+	public void ContinueGame() {
+
+		DisableGateCollision ();
+		Invoke ("EnableGateCollision", 3);
+	}
+
+	public void UpdateLifeBoard() {
+		LifeBoard.text = "Life: " + Life;
+	}
+
+	public void UpdateScoreBoard() {
+		ScoreBoard.text = "Score: " + Score;
+	}
+
+
+}
---
+++ Pinball/Assets/Scripts/InputManager.cs
@@ -0,0 +1,109 @@
+﻿#if UNITY_ANDROID
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	public float torqueForce;
+
+	public GameObject leftFlipper;
+	public GameObject rightFlipper;
+
+	private Rigidbody2D leftFlipperRigid;
+	private Rigidbody2D rightFlipperRigid;
+
+	private SpriteRenderer leftFlipperSpriteRenderer;
+	private SpriteRenderer rightFlipperSpriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+
+		// Set up flippers Rigidbody2D
+		leftFlipperRigid = leftFlipper.GetComponent<Rigidbody2D>();
+		rightFlipperRigid = rightFlipper.GetComponent<Rigidbody2D>();
+
+		// Set up flippers SpritesRenderer
+		leftFlipperSpriteRenderer = leftFlipper.GetComponent<SpriteRenderer>();
+		rightFlipperSpriteRenderer = rightFlipper.GetComponent<SpriteRenderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			Vector3 mouseInput = Input.mousePosition;
+			//Flipping right
+			if (mouseInput.x >= Screen.width / 2f)
+			{
+				AddTorque(rightFlipperRigid, -torqueForce);
+			}
+
+			//Flipping left
+			if (mouseInput.x < Screen.width / 2f)
+			{
+				AddTorque(leftFlipperRigid, torqueForce);
+			}
+		}
+	}
+	void AddTorque(Rigidbody2D rigid, float force)
+	{
+		rigid.AddTorque(force);
+	}
+
+}
+
+#endif
+
+
+
+#if UNITY_STANDALONE_WIN
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	public float torqueForce;
+
+	public GameObject leftFlipper;
+	public GameObject rightFlipper;
+
+	private Rigidbody2D leftFlipperRigid;
+	private Rigidbody2D rightFlipperRigid;
+
+	private SpriteRenderer leftFlipperSpriteRenderer;
+	private SpriteRenderer rightFlipperSpriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+
+		// Set up flippers Rigidbody2D
+		leftFlipperRigid = leftFlipper.GetComponent<Rigidbody2D>();
+		rightFlipperRigid = rightFlipper.GetComponent<Rigidbody2D>();
+
+		// Set up flippers SpritesRenderer
+		leftFlipperSpriteRenderer = leftFlipper.GetComponent<SpriteRenderer>();
+		rightFlipperSpriteRenderer = rightFlipper.GetComponent<SpriteRenderer>();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+			AddTorque (rightFlipperRigid, -torqueForce);
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+			AddTorque(leftFlipperRigid, torqueForce);
+	}
+
+	void AddTorque(Rigidbody2D rigid, float force)
+	{
+		rigid.AddTorque(force);
+	}
+
+}
+
+#endif
